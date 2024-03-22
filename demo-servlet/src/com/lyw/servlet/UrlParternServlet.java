ce3843b926d4e2fe6f75f08966abe31cd25f3376
liuyaowen <liuyaowen_smile@126.com>
2024-03-22 09:27:29 +0800

    url-partern写法

---
+++ demo-servlet/src/com/lyw/servlet/UrlParternServlet.java
@@ -0,0 +1,17 @@
+package com.lyw.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class UrlParternServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("OK");
+
+
+    }
+}
